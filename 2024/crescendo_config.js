47ce79d46d58fe24d9aed8ff1d9c215b88b2f199
TahmidAmbia <162759100+TahmidAmbia@users.noreply.github.com>
2024-03-09 20:17:17 -0500

    Update crescendo_config.js
    changed to milford event

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024isde1",
+      "defaultValue": "2024mimil",
       "required": "true"
     },
     { "name": "Match Level",
