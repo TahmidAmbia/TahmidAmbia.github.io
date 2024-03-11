cad780a994ae0460db2857759084e2fb9f791c3e
TahmidAmbia <162759100+TahmidAmbia@users.noreply.github.com>
2024-03-11 18:34:47 -0400

    Update crescendo_config.js
    change curie to milford 24

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023cur",
+      "defaultValue": "2024mimil",
       "required": "true"
     },
     { "name": "Match Level",
