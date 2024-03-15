ae3c71c8964d9fb61bf7e1873e074980af2078b0
TahmidAmbia <162759100+TahmidAmbia@users.noreply.github.com>
2024-03-15 18:08:59 -0400

    Update crescendo_config.js

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -99,7 +99,7 @@ var config_data = `
       "filename": "2024/field_image.png",
       "clickRestriction": "one",
       "allowableResponses": "2 6 7 11 18 19 26 30 31 35 38 42 43 47 54 55 66 67",
-      "shape": "circle 5 black green true"
+      "shape": "circle 10 black green true"
     }
   ],
   "teleop": [
