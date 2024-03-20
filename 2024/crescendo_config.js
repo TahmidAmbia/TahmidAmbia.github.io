8e534660568310daa2f38f70f60637bac006f060
TahmidAmbia <162759100+TahmidAmbia@users.noreply.github.com>
2024-03-20 17:15:47 -0400

    Update crescendo_config.js
    removed some climbing options

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -156,9 +156,6 @@ var config_data = `
       "choices": {
         "p": "Parked<br>",
         "o": "Onstage<br>",
-        "s": "Onstage (Spotlit)<br>",
-        "h": "Harmony<br>",
-        "a": "Attempted but failed<br>",
         "x": "Not attempted"
       },
       "defaultValue": "x"
