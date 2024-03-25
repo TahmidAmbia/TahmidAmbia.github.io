3e48dd87777f82ca470519acdac2589112da6dfa
TahmidAmbia <162759100+TahmidAmbia@users.noreply.github.com>
2024-03-25 15:18:26 -0400

    Update crescendo_config.js
    macomb prescout

--- 2024/crescendo_config.js
+++ 2024/crescendo_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2024mitry",
+      "defaultValue": "prescoutmacomb",
       "required": "true"
     },
     { "name": "Match Level",
